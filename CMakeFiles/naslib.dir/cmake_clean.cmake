3be2499f986194e7fa6efb03b606b54fb4181eac
prilepski <dmitriprilepski@gmail.com>
2017-09-30 09:07:11 -0400

    initial commit. Not working as there is no submodules defined

---
+++ CMakeFiles/naslib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/naslib.dir/naslib.cpp.o"
+  "build/naslib.pdb"
+  "build/naslib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/naslib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
